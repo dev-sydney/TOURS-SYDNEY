a662ca67bd13dfe4a3fbbe5265cc1339f8008e9f
dev-sydney <sydneyotutey279@gmail.com>
2022-04-11 09:30:20 +0000

    Implemented the route handler function for renderring the login form

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -15,12 +15,22 @@ exports.getTour = catchAsyncErrors(async (req, res, next) => {
   //TODO:
   //1) Get the data for the requested tour (including the reviews & guides)
   const tour = await Tour.findOne({ slug: req.params.tourSlug }).populate({
-    path: 'reviews',
+    path: 'reviews guides',
     fields: 'reviews rating user',
   });
 
   //2) Build the template
 
   //3)Render the template using the data from 1)
-  res.status(200).render('tour', { title: 'The Forest Hiker Tour', tour });
+  res
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .status(200)
+    .render('tour', { title: `${tour.name} tour`, tour });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', { title: 'Log into your account' });
+};
